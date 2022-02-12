1ca7b565f37d4795cbabc33ce1851c234597e45a
rabos23 <rabos23@gmail.com>
2022-02-12 19:07:27 +0100

    init

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useSelector } from "../hooks/useTypeSelector";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector((state) => state.repositories);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchRepositories(term);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
---
+++ src/state/actions/index.ts
@@ -0,0 +1,19 @@
+import { ActionType } from "../action-types";
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepositoriesErorrAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+export type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErorrAction;
